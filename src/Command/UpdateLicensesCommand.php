7b5eebde9f2159f75027ed898e34a4e942e94111
SebSept <sebastienmonterisi@yahoo.fr>
2020-10-14 18:34:32 +0200

    replace self::<constant> by static::<constant>
    So that a command can extend UpdateLicensesCommand and define new values for this constants.

--- src/Command/UpdateLicensesCommand.php
+++ src/Command/UpdateLicensesCommand.php
@@ -118,7 +118,7 @@ class UpdateLicensesCommand extends Command
                 null,
                 InputOption::VALUE_REQUIRED,
                 'License file to apply',
-                realpath(self::DEFAULT_LICENSE_FILE)
+                realpath(static::DEFAULT_LICENSE_FILE)
             )
             ->addOption(
                 'target',
@@ -131,14 +131,14 @@ class UpdateLicensesCommand extends Command
                 null,
                 InputOption::VALUE_REQUIRED,
                 'Comma-separated list of folders and files to exclude from the update',
-                implode(',', self::DEFAULT_FILTERS)
+                implode(',', static::DEFAULT_FILTERS)
             )
             ->addOption(
                 'extensions',
                 null,
                 InputOption::VALUE_REQUIRED,
                 'Comma-separated list of file extensions to update',
-                implode(',', self::DEFAULT_EXTENSIONS)
+                implode(',', static::DEFAULT_EXTENSIONS)
             )
             ->addOption(
                 'display-report',
