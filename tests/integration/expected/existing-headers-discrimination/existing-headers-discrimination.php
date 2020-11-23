8d3fb55aa9379f31baae656e90b510a8bb2e4b3a
SebSept <sebastienmonterisi@yahoo.fr>
2020-11-23 23:18:08 +0100

    test now fails as expected.
    header not replaced in existing-headers-discrimination.php

--- tests/integration/expected/existing-headers-discrimination/existing-headers-discrimination.php
+++ tests/integration/expected/existing-headers-discrimination/existing-headers-discrimination.php
@@ -1,11 +1,21 @@
 <?php
 /**
- * With the new option this header will be fixed.
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
  *
- * Currently it is not because it does not containt the word 'presta*hop' (notice the S replaced by *).
+ * NOTICE OF LICENSE
  *
- * With the option initialized with 'friendsofpresta'
- * this file will be fixed and not others
+ * This source file is subject to the Academic Free License 3.0 (AFL-3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/AFL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License 3.0 (AFL-3.0)
  */
 
 class existingHeaderDiscrimination extends Module
